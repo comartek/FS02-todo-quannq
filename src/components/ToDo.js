8a951ec547b3cdd2e077e073eb98d0f5e281a01e
Comartek <comartek@Comarteks-Mac-mini.local>
2022-08-18 16:52:49 +0700

    Todolist

---
+++ src/components/ToDo.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from "@mui/icons-material/Clear";
+
+import "./ToDo.css";
+
+const ToDo = () => {
+  const [enterContent, setEnterContent] = useState("");
+  const [enterDate, setEnterDate] = useState("");
+
+  const [storeValue, setStoreValue] = useState([{}]);
+
+  const enterContentHandler = (e) => {
+    setEnterContent(e.target.value);
+  };
+
+  const enterDateHandler = (e) => {
+    setEnterDate(e.target.value);
+  };
+
+  const storeValueHandler = () => {
+    setStoreValue([...storeValue, { Content: enterContent, Date: enterDate }]);
+  };
+
+  const deleteItem = () => {
+    setStoreValue([...storeValue]);
+  };
+
+  return (
+    <div className="toDo">
+      <div className="header">
+        <div className="header__icon">
+          <div className="header__input">
+            <form>
+              <label htmlFor="content">Content</label>
+              <input
+                type="text"
+                value={enterContent}
+                id="content"
+                name="content"
+                onChange={enterContentHandler}
+              />
+              <label htmlFor="date">Date</label>
+              <input
+                type="date"
+                id="date"
+                name="date"
+                value={enterDate}
+                onChange={enterDateHandler}
+              />
+            </form>
+          </div>
+          <button onClick={storeValueHandler}>
+            <AddIcon />
+          </button>
+        </div>
+        <div className="header__table">
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Content</th>
+                <th>Date</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {storeValue.map((st, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{st.Content}</td>
+                    <td>{st.Date}</td>
+                    <td>
+                      <button onClick={deleteItem}>
+                        <CheckIcon />
+                      </button>
+                      <button onClick={deleteItem}>
+                        <ClearIcon />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToDo;
